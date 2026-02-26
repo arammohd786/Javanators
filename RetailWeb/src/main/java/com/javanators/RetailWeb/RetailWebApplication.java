3f4cb2523bde27d5a7177ea7ee215346e5033d32
aram <arammohd786@gmail.com>
2026-02-26 11:22:03 +0530

    adding project

---
+++ RetailWeb/src/main/java/com/javanators/RetailWeb/RetailWebApplication.java
@@ -0,0 +1,13 @@
+package com.javanators.RetailWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailWebApplication.class, args);
+	}
+
+}
---
+++ RetailWeb/src/test/java/com/javanators/RetailWeb/RetailWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanators.RetailWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
